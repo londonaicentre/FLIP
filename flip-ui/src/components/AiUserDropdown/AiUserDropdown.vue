657d0380a713d04cd9e017ed35eee1033e65ceb1
at24_bioeng625-pc <alexandre.triay_bagur@kcl.ac.uk>
2026-07-08 01:32:06 +0100

    fix(flip-ui): regular-weight icons in the account dropdown
    The theme toggle and Change Password items drop their duotone fills
    (sun/lock), and the dark-mode toggle's moon-stars glyph becomes the plain
    moon. Sign Out already used a non-duotone glyph.
    
    Signed-off-by: at24_bioeng625-pc <alexandre.triay_bagur@kcl.ac.uk>

--- flip-ui/src/components/AiUserDropdown/AiUserDropdown.vue
+++ flip-ui/src/components/AiUserDropdown/AiUserDropdown.vue
@@ -74,8 +74,8 @@
                             data-test="sign-out-btn"
                             @click="emit('toggleDarkMode')"
                         >
-                            <icon-ph-sun-duotone v-if="isDark" class="dark:group-hover:text-yellow-200 h-5 w-5 mr-3 text-gray-500 dark:text-gray-300 transition group-hover:text-gray-600" />
-                            <icon-ph-moon-stars-duotone v-else class="h-5 w-5 mr-3 text-gray-500 dark:text-gray-300 transition group-hover:text-gray-600 dark:group-hover:text-gray-200" />
+                            <icon-ph-sun v-if="isDark" class="dark:group-hover:text-yellow-200 h-5 w-5 mr-3 text-gray-500 dark:text-gray-300 transition group-hover:text-gray-600" />
+                            <icon-ph-moon v-else class="h-5 w-5 mr-3 text-gray-500 dark:text-gray-300 transition group-hover:text-gray-600 dark:group-hover:text-gray-200" />
                             {{ isDark ? 'Light Mode' : "Dark Mode" }}
                         </button>
                     </MenuItem>
@@ -90,7 +90,7 @@
                             data-test="change-password-btn"
                             @click="changePassword"
                         >
-                            <icon-ph-lock-duotone
+                            <icon-ph-lock
                                 class="w-5 h-5 mr-3 text-gray-500 dark:text-gray-300 transition group-hover:text-gray-600 dark:group-hover:text-gray-200"
                                 aria-hidden="true"
                             />
